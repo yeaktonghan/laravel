92a1c9ff0431432d626c45244f1cfd464b67b5f7
yeaktonghan <yeaktonghan.github@gmail.com>
2022-09-13 18:33:57 +0700

    Start changing code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
